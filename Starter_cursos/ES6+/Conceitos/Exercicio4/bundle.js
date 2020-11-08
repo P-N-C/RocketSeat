2bb3606df1abccf576df652f95cecdcf86ea031a
Pedro Nogueira <pnc@cin.ufpe.br>
2020-11-08 15:13:03 -0300

    Exercicios dos Conteitos de ES6+ completos

---
+++ Starter_cursos/ES6+/Conceitos/Exercicio4/bundle.js
@@ -0,0 +1,28 @@
+"use strict";
+
+//4.1
+var empresa = {
+  nome: 'Rocketseat',
+  endereco: {
+    cidade: 'Rio do Sul',
+    estado: 'SC'
+  }
+};
+var nome = empresa.nome,
+    _empresa$endereco = empresa.endereco,
+    cidade = _empresa$endereco.cidade,
+    estado = _empresa$endereco.estado;
+console.log(nome);
+console.log(cidade);
+console.log(estado); //4.2
+
+function mostraInfo(_ref) {
+  var nome = _ref.nome,
+      idade = _ref.idade;
+  return "".concat(nome, " tem ").concat(idade, " anos.");
+}
+
+console.log(mostraInfo({
+  nome: 'Diego',
+  idade: 23
+}));
---
+++ Starter_cursos/ES6+/Conceitos/Exercicio7/bundle.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var nome = 'Diego';
+var idade = 23;
+var cidade = 'Rio do Sul';
+var usuario = {
+  nome: nome,
+  idade: idade,
+  cidade: cidade
+};
+console.log(usuario);
